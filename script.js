b1fa35c44270ec3c522d75903e597a497aaace8a
Anwar_Hossain <ranakrphone@gmail.com>
2024-09-23 04:28:13 +0600

    validation part complete

--- script.js
+++ script.js
@@ -24,27 +24,37 @@ const donateBtn3 = document.getElementById("donateBtn3");
 
 donateBtn1.addEventListener("click", function (e) {
   const inputAmountId1 = getInputFieldValueById("inputAmountId1");
-  //   console.log(inputAmountId1);
+  //   console.log(typeof inputAmountId1);
 
   if (inputAmountId1 < 0 || isNaN(inputAmountId1)) {
     return alert("Invalid input");
   }
 
+  if (mainAmount.innerText < inputAmountId1) {
+    return alert(`You cannot donate more than ${mainAmount.innerText} tk.`);
+  }
+
   const donateAmount1 = document.getElementById("donateAmount1");
-  //   console.log(donateAmount1.innerText);
-  donateAmount1.innerText = inputAmountId1;
+  donateAmount1.innerText =
+    parseFloat(donateAmount1.innerText) + inputAmountId1.toFixed(2);
+  //   console.log(typeof parseFloat(donateAmount1.innerText));
 
   //   console.log(parseFloat(mainAmount.innerText));
-  mainAmount.innerText -= inputAmountId1;
+  mainAmount.innerText -= inputAmountId1.toFixed(2);
+
+  my_modal_5.showModal();
 
   const noakhaliFloodEl = getHtmlElementById("noakhaliFlood");
 
   const div = `<div class="border rounded-md p-6 space-y-4">
-          <h5>${inputAmountId1} Taka is ${noakhaliFloodEl.innerText}</h5>
+          <h5>${inputAmountId1.toFixed(2)} Taka is ${
+    noakhaliFloodEl.innerText
+  }</h5>
           <p class="font-extralight">Date: ${new Date().toString()}</p>
         </div>`;
 
   historyPartIdEl.innerHTML += div;
+  document.getElementById("inputAmountId1").value = "";
 });
 
 donateBtn2.addEventListener("click", function (e) {
@@ -55,21 +65,28 @@ donateBtn2.addEventListener("click", function (e) {
     return alert("Invalid input");
   }
 
+  if (mainAmount.innerText < inputAmountId2) {
+    return alert(`You cannot donate more than ${mainAmount.innerText} tk.`);
+  }
+
   const donateAmount2 = document.getElementById("donateAmount2");
   //   console.log(donateAmount2.innerText);
-  donateAmount2.innerText = inputAmountId2;
+  donateAmount2.innerText = inputAmountId2.toFixed(2);
 
   //   console.log(parseFloat(mainAmount.innerText));
-  mainAmount.innerText -= inputAmountId2;
+  mainAmount.innerText -= inputAmountId2.toFixed(2);
+
+  my_modal_5.showModal();
 
   const feniFloodEl = getHtmlElementById("feniFlood");
 
   const div = `<div class="border rounded-md p-6 space-y-4">
-          <h5>${inputAmountId2} Taka is ${feniFloodEl.innerText}</h5>
+          <h5>${inputAmountId2.toFixed(2)} Taka is ${feniFloodEl.innerText}</h5>
           <p class="font-extralight">Date: ${new Date().toString()}</p>
         </div>`;
 
   historyPartIdEl.innerHTML += div;
+  document.getElementById("inputAmountId2").value = "";
 });
 
 donateBtn3.addEventListener("click", function (e) {
@@ -80,21 +97,28 @@ donateBtn3.addEventListener("click", function (e) {
     return alert("Invalid input");
   }
 
+  if (mainAmount.innerText < inputAmountId3) {
+    return alert(`You cannot donate more than ${mainAmount.innerText} tk.`);
+  }
+
   const donateAmount3 = document.getElementById("donateAmount3");
   //   console.log(donateAmount3.innerText);
-  donateAmount3.innerText = inputAmountId3;
+  donateAmount3.innerText = inputAmountId3.toFixed(2);
 
   //   console.log(parseFloat(mainAmount.innerText));
-  mainAmount.innerText -= inputAmountId3;
+  mainAmount.innerText -= inputAmountId3.toFixed(2);
+
+  my_modal_5.showModal();
 
   const quotaAidEl = getHtmlElementById("quotaAid");
 
   const div = `<div class="border rounded-md p-6 space-y-4">
-          <h5>${inputAmountId3} Taka is ${quotaAidEl.innerText}</h5>
+          <h5>${inputAmountId3.toFixed(2)} Taka is ${quotaAidEl.innerText}</h5>
           <p class="font-extralight">Date: ${new Date().toString()}</p>
         </div>`;
 
   historyPartIdEl.innerHTML += div;
+  document.getElementById("inputAmountId3").value = "";
 });
 
 const blogIdEl = getHtmlElementById("blogId");
